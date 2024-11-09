7ea043736325a2df9112a86cb55ce24a12b30e13
sayandas-sd <sayandasmac@gmail.com>
2024-11-09 20:55:33 +0530

    creating connecton to devnet and airdrop

--- create-nft.ts
+++ create-nft.ts
@@ -5,5 +5,10 @@ import {clusterApiUrl, Connection, LAMPORTS_PER_SOL} from "@solana/web3.js";
 
 const connection = new Connection(clusterApiUrl("devnet"));
 const user = await getKeypairFromFile();
-
+await airdropIfRequired(
+    connection,
+    user.publicKey,
+    1 * LAMPORTS_PER_SOL,
+    0.5 * LAMPORTS_PER_SOL,
+);
 
