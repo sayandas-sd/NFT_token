43c701281b2f27cb146c05e6af62dc56bd930ad2
sayandas-sd <sayandasmac@gmail.com>
2024-11-09 21:02:16 +0530

    added umi instance

--- create-nft.ts
+++ create-nft.ts
@@ -2,6 +2,8 @@ import { createNft, fetchDigitalAsset, mplTokenMetadata } from "@metaplex-founda
 import { airdropIfRequired, getExplorerLink, getKeypairFromFile } from "@solana-developers/helpers";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import {clusterApiUrl, Connection, LAMPORTS_PER_SOL} from "@solana/web3.js";
+import { keypairIdentity } from "@metaplex-foundation/umi";
+
 
 const connection = new Connection(clusterApiUrl("devnet"));
 const user = await getKeypairFromFile();
@@ -12,3 +14,13 @@ await airdropIfRequired(
     0.5 * LAMPORTS_PER_SOL,
 );
 
+console.log("user key", user.publicKey.toBase58());
+
+
+const umi = createUmi(connection.rpcEndpoint);
+umi.use(mplTokenMetadata());
+
+const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+umi.use(keypairIdentity(umiUser))
+
+console.log("set up the umi");
